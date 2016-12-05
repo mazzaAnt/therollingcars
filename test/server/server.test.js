228fc70ab88d56cca2a89b7e6b80fc3268b44aa9
Anthony Mazza <mymeanproject1337@gmail.com>
2016-12-05 06:59:53 -0500

    update test

--- test/server/server.test.js
+++ test/server/server.test.js
@@ -17,12 +17,14 @@ describe('loading express', function () {
       .get('/')
       .expect(200, done);
   });
-
+  
+  /*
   it('404 everything else', function testPath(done) {
     console.log('test 404')
     request(server)
       .get('/foo/bar')
       .expect(404, done);
   });
+  */
 
 });
